3f8bbe0db16e67fa55a4f4eb8d01d0daddab188b
tiurmagrace <tiursihaloho210905@gmail.com>
2025-03-11 00:55:37 +0700

    Membersihkan file yang tidak perlu dan memperbaiki .gitignore

--- tjmod4_2311104042/Program.cs
+++ tjmod4_2311104042/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class KodeBuah
+{
+    public string GetKodeBuah(string namaBuah)
+    {
+        switch (namaBuah.ToLower())
+        {
+            case "apel":
+                return "A00";
+            case "aprikot":
+                return "B00";
+            case "alpukat":
+                return "C00";
+            case "pisang":
+                return "D00";
+            case "paprika":
+                return "E00";
+            case "blackberry":
+                return "F00";
+            case "cherry":
+                return "H00";
+            case "kelapa":
+                return "J00";
+            case "kurma":
+                return "K00";
+            case "durian":
+                return "L00";
+            case "anggur":
+                return "M00";
+            case "melon":
+                return "N00";
+            case "semangka":
+                return "O00";
+            default:
+                return "Kode tidak ditemukan";
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodeBuah kodeBuah = new KodeBuah();
+        Console.WriteLine("Masukkan nama buah:");
+        string namaBuah = Console.ReadLine();
+        string kode = kodeBuah.GetKodeBuah(namaBuah);
+        Console.WriteLine($"Kode untuk {namaBuah} adalah: {kode}");
+    }
+}
